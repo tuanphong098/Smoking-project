7a0f3149770e41ef56ec7935c688f39d81aab6d2
phong <phongntse184892@fpt.edu.vn>
2025-06-01 18:43:00 +0700

    Init project and add category functionalities

---
+++ src/test/java/edu/fpt/smokingcessationplatform/SmokingCessationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fpt.smokingcessationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokingCessationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
